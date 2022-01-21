4e9e6ca22e1d4142cdfe36d2186c9d13b02d80c3
MerkulovKA <kirill_tagil9@mail.ru>
2022-01-21 18:49:16 +0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ["IBM Plex Sans"]
+      }
+    },
+    colors: {
+      'orange': '#F59C00',
+    }
+  },
+  plugins: [],
+}
