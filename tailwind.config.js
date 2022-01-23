cdbfffb13782f08889370850f56815d03219f3fa
MerkulovKA <kirill_tagil9@mail.ru>
2022-01-23 10:33:28 +0500

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,16 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      fontFamily: {
-        'sans': ["IBM Plex Sans"]
-      }
+      backgroundImage: {
+        'main': "url('../src/img/main.png')",
+        'mobile': "url('../src/img/mobile.png')",
+      },
     },
     colors: {
+      'white': '#ffffff',
       'orange': '#F59C00',
+      'gray': '#242046',
+      'blue': '#3A3186',
     }
   },
   plugins: [],
